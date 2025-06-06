53322cd5803d8f04dca26117b5508b8fc200b85b
Madhav Paudel <poudelmadhav143@gmail.com>
2025-06-06 08:14:19 +0545

    test and lint

--- rails-auth-server/config/initializers/cors.rb
+++ rails-auth-server/config/initializers/cors.rb
@@ -1,11 +1,11 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:12000', 'http://localhost:9000' # your SPA origin
+    origins "http://localhost:12000", "http://localhost:9000" # your SPA origin
 
-    resource '*',
+    resource "*",
       headers: :any,
-      methods: [:get, :post, :options],
+      methods: [ :get, :post, :options ],
       credentials: false
   end
 end
